c5e94d7b2a60789e5d80d9ca009a69c155cdfe21
mintergraphsystems <mintergraphsystems@gmail.com>
2025-08-12 16:29:12 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -24,28 +24,28 @@ export const PRODUCTS: Product[] = [
 export const TESTIMONIALS: Testimonial[] = [
   {
     quote: "We, at EMRI Green Healthcare, Jaipur, would like to extend our heartfelt appreciation to Mintergraph Systems Pvt. Ltd. for your outstanding support and technical expertise in the successful execution of key deliverables under the DIAL 112 Project for Rajasthan Police.",
-    author: 'Mohit Kumar Jain',
+    author: 'Mr.Mohit Kumar Jain',
     company: 'Enterprise Client',
   },
   {
     quote: "Eastern Railway, Asansol Division extends its special thanks to Mintergraph Systems Pvt. Ltd. for delivering excellent services that ensure the smooth functioning of UTS and PRS systems. The timely support provided in addressing complaints is truly appreciated by all teams.",
-    author: 'Dipak Mondal',
-    company: 'North Eastern Railway, Izzatnagar Division',
+    author: 'Mr.Dipak Mondal',
+    company: 'Eastern Railway, Asansol Division',
   },
   {
     quote: "The North Eastern Railway, Izzatnagar Division, extends its special thanks to Mr. Devesh Singh, Senior Project Engineer at M Intergraph Systems Limited, for his exceptional knowledge and problem-solving abilities. His outstanding service has played a significant role in ensuring the smooth operation of the UTS (Unreserved Ticketing System) and PRS (Passenger Reservation System).",
-    author: 'Sanjeev Banerjee',
-    company: 'North Eastern Railway',
+    author: 'Mr.Sanjeev Banerjee',
+    company: 'North Eastern Railway',Izzatnagar Division
   },
   {
     quote: "M/s. M Intergraph Systems Pvt. Ltd. supplied and installed 138 Thin Clients (OEM – Thinvent) at Patna Jn. in 2021. The deployment was smooth, and all units performed reliably throughout the 3-year warranty. Highly appreciated and recommended!",
-    author: 'Chandra Shekar Prasad',
+    author: 'Mr.Chandra Shekar Prasad',
     company: 'East Central Railway, Patna Division',
 
   },
  {
     quote: "M/s. M Intergraph Systems Pvt. Ltd. supplied and installed 138 Thin Clients (OEM – Thinvent) at Patna Jn. in 2021. The deployment was smooth, and all units performed reliably throughout the 3-year warranty. Highly appreciated and recommended!",
-    author: 'Himanshu Shukla',
+    author: 'Mr.Himanshu Shukla',
     company: 'East Central Railway, Patna Division',
   },
 
