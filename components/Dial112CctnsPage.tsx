1fc5d3d66537d7f31c48164db72e2a7c78c0fd3b
mintergraphsystems <mintergraphsystems@gmail.com>
2025-08-12 15:32:05 +0530

    Update Dial112CctnsPage.tsx

--- components/Dial112CctnsPage.tsx
+++ components/Dial112CctnsPage.tsx
@@ -75,11 +75,12 @@ export const Dial112CctnsPage: React.FC<Dial112CctnsPageProps> = ({ onNavigate }
                         <div className="relative animate-on-scroll" style={{'--animation-delay': '0.1s'} as React.CSSProperties}>
                     <!-- direct SVG (vector) -->
 <img
-  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Emergency_telephone_number_112.svg/291px-Emergency_telephone_number_112.svg.png"
-  alt="112 emergency phone icon"
+  src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/SOS_112.svg/642px-SOS_112.svg.png"
+  alt="SOS 112 emergency icon"
   className="rounded-2xl shadow-2xl aspect-[4/3] object-cover"
   loading="lazy" decoding="async"
 />
+
 
 
 
